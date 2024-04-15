d3ba88310a8703c9db3b0c85b92e4c7d8625d489
Emssui <eames.moller@gmail.com>
2024-04-15 10:14:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Test");
+
+            int age = 23;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            long hugNumber = 90000000000L;
+            Console.WriteLine(hugNumber);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
